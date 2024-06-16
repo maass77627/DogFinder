b270d7db3b8e4b1b903d413c7f08c674f59c60e4
kelly_maass <kmaass77627@gmail.com>
2024-06-16 18:53:40 -0500

    done

--- index.js
+++ index.js
@@ -12,12 +12,28 @@ function createDog(dog) {
     console.log(dog)
     let body = document.querySelector("body")
     let div = document.createElement("div")
+    div.id = "dog-container"
     let h1 = document.createElement("h1")
     let p = document.createElement("p")
+    let button = document.createElement("button")
+    let buttontwo = document.createElement("button")
+    buttontwo.innerHTML = "new dog"
+    buttontwo.addEventListener('click', (e) => getNewDog(e))
+    button.innerHTML = "Like"
     let image = document.createElement("img")
     image.src = dog.message
     div.appendChild(image)
+    div.appendChild(button)
+    div.appendChild(buttontwo)
     body.appendChild(div)
+
+}
+
+function getNewDog(e) {
+    
+fetch('https://dog.ceo/api/breeds/image/random')
+.then((response) => response.json())
+.then((json) => createDog(json))
 }
 
 
